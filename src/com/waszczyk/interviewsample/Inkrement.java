41a3fa05c219a7f3dd7db56319360c5116f9d5d1
Tomasz Waszczyk <tomasz@waszczyk.com>
2015-03-28 18:58:11 +0100

    [Added] Inkrement.java

---
+++ src/com/waszczyk/interviewsample/Inkrement.java
@@ -0,0 +1,11 @@
+package com.waszczyk.interviewsample;
+
+
+public class Inkrement {
+
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(i++ + ++i);
+    }
+
+}
